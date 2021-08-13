3c56de28443e17bcdc905a057d5591bec615e848
Gugnu <lochammehtab@gmail.com>
2021-08-12 23:12:28 -0400

    used inquirer npm for prompt and promise

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js')
-const profileDataArgs = process.argv.slice(2);
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js')
+
 
 // const printProfileData = profileDataArr => {
 //     // This...
@@ -24,10 +25,25 @@ const profileDataArgs = process.argv.slice(2);
   // const name = profileDataArgs[0];
   // const github = profileDataArgs[1];
   
-  const [name, github] = profileDataArgs;
+
+
+
+  // const pageHTML = generatePage(name, github);
   
-  fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+  // fs.writeFile('./index.html', generatePage(name, github), err => {
+  //   if (err) throw new Error(err);
   
-    console.log('Portfolio complete! Check out index.html to see the output!');
-  });
+  //   console.log('Portfolio complete! Check out index.html to see the output!');
+  // });
+
+  const promptUser = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      }
+    ]);
+  };
+  
+  promptUser().then(answers => console.log(answers));
