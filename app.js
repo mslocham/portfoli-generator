ca6168b7f0bb74d4f2ff73167121677521c72c3c
Gugnu <lochammehtab@gmail.com>
2021-08-14 16:11:17 -0400

    .filter, .map, spread and rest operators

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js')
+const fs = require('fs');
+const generatePage = require('./src/page-template.js')
 
 
 // const printProfileData = profileDataArr => {
@@ -168,9 +168,14 @@ const inquirer = require('inquirer');
     })
   };
 
-
   promptUser()
     .then(promptProject)
     .then(portfolioData => {
-    console.log(portfolioData);
+      const pageHTML = generatePage(portfolioData);
+
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
   });
